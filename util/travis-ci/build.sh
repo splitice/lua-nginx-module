4f788e1a4d5f9e5d086b22d7472d8f0f89e22275
splitice <mat999@gmail.com>
2015-05-09 23:05:35 +1000

    fix

--- util/travis-ci/build.sh
+++ util/travis-ci/build.sh
@@ -18,7 +18,7 @@ echo "Compiling into $OUT as $WHOAMI";
 function install_github {
 	wget https://github.com/$1/$2/archive/$3.tar.gz
 	tar -zxvf $3.tar.gz
-	if [[ "z$4" != "z" ]]; then
+	if [[ "z$4" == "z" ]]; then
 		ADDITIONAL_CONFIGURE="$ADDITIONAL_CONFIGURE --add-module=$OUT/$3"
 	fi
 }
