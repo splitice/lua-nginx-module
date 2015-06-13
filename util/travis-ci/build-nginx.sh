6df6a3f1c4b59fcb9490d2a34e860b6b2014808f
splitice <mat999@gmail.com>
2015-06-13 18:04:28 +1000

    enable UTF8 PCRE

--- util/travis-ci/build-nginx.sh
+++ util/travis-ci/build-nginx.sh
@@ -77,7 +77,8 @@ env LIBDRIZZLE_LIB=/usr/lib/ LIBDRIZZLE_INC=/usr/include/libdrizzle-1.0 ./config
         --with-http_ssl_module --with-http_stub_status_module --with-openssl=$OUT/$BUILD_OPENSSL_VERSION/ \
         --without-http_uwsgi_module --without-http_scgi_module --with-select_module --with-poll_module \
         --with-http_dav_module --with-http_spdy_module --with-http_gunzip_module --with-http_realip_module \
-		--with-http_auth_request_module --with-http_image_filter_module --with-pcre=$OUT/pcre-$BUILD_PCRE_VERSION --with-pcre-jit \
+		--with-http_auth_request_module --with-http_image_filter_module \
+		--with-pcre=$OUT/pcre-$BUILD_PCRE_VERSION --with-pcre-opt="-enable-utf8" --with-pcre-jit \
 		$ADDITIONAL_CONFIGURE --with-debug
 set +o xtrace
 
