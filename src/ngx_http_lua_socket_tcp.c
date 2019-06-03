4d944fa4167d17a039962f2366134fe84effe687
Mathew Heard <splitice@users.noreply.github.com>
2019-06-03 21:59:57 +1000

    getfd

--- src/ngx_http_lua_socket_tcp.c
+++ src/ngx_http_lua_socket_tcp.c
@@ -20,6 +20,7 @@
 
 
 static int ngx_http_lua_socket_tcp(lua_State *L);
+static int ngx_http_lua_socket_tcp_getfd(lua_State *L);
 static int ngx_http_lua_socket_tcp_connect(lua_State *L);
 #if (NGX_HTTP_SSL)
 static int ngx_http_lua_socket_tcp_sslhandshake(lua_State *L);
@@ -250,6 +251,9 @@ ngx_http_lua_inject_socket_tcp_api(ngx_log_t *log, lua_State *L)
     lua_pushcfunction(L, ngx_http_lua_socket_tcp_settimeouts);
     lua_setfield(L, -2, "settimeouts"); /* ngx socket mt */
 
+    lua_pushcfunction(L, ngx_http_lua_socket_tcp_getfd);
+    lua_setfield(L, -2, "getfd"); /* ngx socket mt */
+
     lua_pushvalue(L, -1);
     lua_setfield(L, -2, "__index");
 
@@ -390,6 +394,29 @@ ngx_http_lua_inject_req_socket_api(lua_State *L)
     lua_setfield(L, -2, "socket");
 }
 
+static int
+ngx_http_lua_socket_tcp_getfd(lua_State *L)
+{
+    ngx_connection_t                    *c;
+    ngx_http_lua_socket_tcp_upstream_t    *u;
+    
+     luaL_checktype(L, 1, LUA_TTABLE);
+
+     lua_rawgeti(L, 1, SOCKET_CTX_INDEX);
+    u = lua_touserdata(L, -1);
+    lua_pop(L, 1);
+
+     if (u == NULL || u->peer.connection == NULL) {
+        lua_pushnil(L);
+        lua_pushliteral(L, "closed");
+        return 2;
+    }
+
+     c = u->peer.connection;
+    lua_pushinteger(L,(int) c->fd);
+
+     return 1;
+}
 
 static int
 ngx_http_lua_socket_tcp(lua_State *L)
