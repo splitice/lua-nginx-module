928e423395ed7b843acb96f719ed5e368e4d9469
Mathew <mat999@gmail.com>
2024-04-27 01:24:52 +1000

    adjust patch for certificate timeout

--- src/ngx_http_lua_ssl_client_helloby.c
+++ src/ngx_http_lua_ssl_client_helloby.c
@@ -219,12 +219,6 @@ ngx_http_lua_ssl_client_hello_handler(ngx_ssl_conn_t *ssl_conn,
     }
 
     dd("first time");
-    
-    // remove hello timer, move to cert timer
-    if(c->read->timer_set) {
-        ngx_del_timer(c->read);
-        ngx_add_timer(c->read, c->listening->ssl_certificate_timeout);
-    }
 
 #if (nginx_version < 1017009)
     ngx_reusable_connection(c, 0);
@@ -255,6 +249,13 @@ ngx_http_lua_ssl_client_hello_handler(ngx_ssl_conn_t *ssl_conn,
     fc->log->file = c->log->file;
     fc->log->log_level = c->log->log_level;
     fc->ssl = c->ssl;
+    
+    cscf = ngx_http_get_module_srv_conf(r, ngx_http_core_module);
+    // remove hello timer, move to cert timer
+    if(c->read->timer_set) {
+        ngx_del_timer(c->read);
+        ngx_add_timer(c->read, cscf->client_ssl_certificate_timeout);
+    }
 
     clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
 
@@ -297,7 +298,6 @@ ngx_http_lua_ssl_client_hello_handler(ngx_ssl_conn_t *ssl_conn,
     c->log->action = "loading SSL client hello by lua";
 
     if (lscf->srv.ssl_client_hello_handler == NULL) {
-        cscf = ngx_http_get_module_srv_conf(r, ngx_http_core_module);
 
         ngx_log_error(NGX_LOG_ALERT, c->log, 0,
                       "no ssl_client_hello_by_lua* defined in "
