93a5ab54a11e1b5f36e08cb88dbd27a1622290bf
Mathew <mat999@gmail.com>
2025-07-08 20:50:20 +1000

    fix staticfile with ssl

--- src/ngx_http_lua_output.c
+++ src/ngx_http_lua_output.c
@@ -78,9 +78,12 @@ int ngx_http_lua_ngx_staticfile_ffi(ngx_http_request_t *r, const char *p, size_t
 
     r->allow_ranges = 0;
     r->single_range = 1;
-    r->filter_need_in_memory = 0;
-    r->main_filter_need_in_memory = 0;
-    r->filter_need_temporary = 0;
+
+    if (r->connection != NULL && r->connection->ssl == NULL) {
+        r->filter_need_in_memory = 0;
+        r->main_filter_need_in_memory = 0;
+        r->filter_need_temporary = 0;
+    }
 
     if (r->stream) {
         r->stream->connection->keepalive = 0;
